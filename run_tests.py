57d734096d27df6351eff8c065cc465b41fd0196
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-11-03 14:26:05 +0100

    automatized run tests

---
+++ run_tests.py
@@ -0,0 +1,7 @@
+import unittest
+from util.csv_manager import CsvManagerTests
+from util.elasticsearch import ElasticsearchTests
+from build_bitext_indexes import _MainTests
+
+if __name__ == '__main__':
+    unittest.main()
