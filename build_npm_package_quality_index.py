db4b0fddb85a63beb236cca5ab111975f9117b1c
DiegoTUI <dlafuente@tuitravel-ad.com>
2014-11-20 16:28:58 +0100

    typo

--- build_npm_package_quality_index.py
+++ build_npm_package_quality_index.py
@@ -92,7 +92,7 @@ class _MainTests(unittest.TestCase):
         global test_packages
         _Main(test = True)
         # iterate documents
-        self.assertTrue(self.elasticsearch.count_documents("test_npm_packages") >= len(test_packages))
+        self.assertTrue(self.elasticsearch.count_documents("test_npm_packages") >= (len(test_packages) - 1))
 
     def tearDown(self):
         # delete indexes
